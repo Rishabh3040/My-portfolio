30e860f670d53fc8b05d82e7013c4da387f39c69
Rishabh Gautam <risabhgautam21016@gmail.com>
2025-05-04 15:29:17 +0530

    two

---
+++ src/components/data/dummydata.js
@@ -0,0 +1,206 @@
+import { Settings, CropRotate, ViewInAr, PieChart, Code, BarChart, CloudOutlined, FavoriteBorder, Public, PersonOutlined, AddLocationAltOutlined, PhoneIphone, EmailOutlined, Facebook, Twitter, Instagram, YouTube } from "@mui/icons-material"
+
+export const navlink = [{
+        url: "/",
+        text: "Home",
+    },
+    {
+        url: "/about",
+        text: "About",
+    },
+    {
+        url: "/services",
+        text: "Services",
+    },
+    {
+        url: "/portfolio",
+        text: "Portfolio",
+    },
+    {
+        url: "/testimonials",
+        text: "Testimonials",
+    },
+    {
+        url: "/blog",
+        text: "Blog",
+    },
+    {
+        url: "/contact",
+        text: "Contact",
+    },
+]
+export const home = [{
+    text: "HELLO I'M",
+    name: "Rishabh Gautam",
+    post: "REACT DEVELOPER",
+    design: "FULL STACK DEVELOPER",
+    desc: "I'm a final-year Computer Science student at AKGEC with a passion for web development. My expertise includes technologies like React.js, Node.js, MongoDB, JavaScript, and TypeScript, C, and C++. I have also honed my problem-solving skills by solving over 200 questions on LeetCode.",
+}, ]
+export const about = [{
+    desc: "I'm a final-year Computer Science student at AKGEC with a passion for web development. My expertise includes technologies like React.js, Node.js, MongoDB, JavaScript, and TypeScript, C, and C++. I have also honed my problem-solving skills by solving over 200 questions on LeetCode.",
+    desc1: "I excel in teamwork, meeting deadlines, and quickly learning new technologies. If you're looking for a dedicated team member for your project, feel free to reach out. I'm always ready for new challenges and eager to contribute to exciting initiatives.",
+    cover: "./images/profile1.png",
+}, ]
+export const services = [{
+        id: 1,
+        icon: < Settings / > ,
+        title: "Creative Design",
+        desc: "Build dynamic, scalable, and performant web apps tailored to client needs using React, Redux, and modern JavaScript.",
+    },
+    {
+        id: 2,
+        icon: < CropRotate / > ,
+        title: "Clean Code",
+        desc: "Develop reusable component libraries or design systems to ensure consistency and efficiency in large-scale projects.",
+    },
+    {
+        id: 3,
+        icon: < ViewInAr / > ,
+        title: "Responsive Design",
+        desc: "Creating a responsive React website involves building a web application using React that adapts smoothly to different screen sizes and devices like mobile phones, tablets, and desktops. Here's a high-level overview and starter code to get you going.",
+    },
+]
+export const project = [{
+        id: 1,
+        icon: < CloudOutlined / > ,
+        num: "89",
+        title: "HAPPY CLIENTS",
+    },
+    {
+        id: 2,
+        icon: < FavoriteBorder / > ,
+        num: "231",
+        title: "PROJECTS COMPLEATED",
+    },
+    {
+        id: 3,
+        icon: < Public / > ,
+        num: "108",
+        title: "FILES DOWNLOADED",
+    },
+    {
+        id: 4,
+        icon: < PersonOutlined / > ,
+        num: "1446",
+        title: "LIENS OF CODE",
+    },
+]
+export const portfolio = [{
+        id: 1,
+        cover: "../images/port/port1.jpg",
+        name: "Brand",
+        category: "marketing",
+        title: "Brex Logo",
+    },
+    {
+        id: 2,
+        cover: "../images/port/port2.jpg",
+        name: "Brand",
+        category: "design",
+        title: "Brex Logo",
+    },
+    {
+        id: 3,
+        cover: "../images/port/port3.jpg",
+        name: "Brand",
+        category: "development",
+        title: "Brex Logo",
+    },
+    {
+        id: 4,
+        cover: "../images/port/port4.jpg",
+        name: "Brand",
+        category: "marketing",
+        title: "Brex Logo",
+    },
+    {
+        id: 5,
+        cover: "../images/port/port5.jpg",
+        name: "Brand",
+        category: "design",
+        title: "Brex Logo",
+    },
+    {
+        id: 6,
+        cover: "../images/port/port6.jpg",
+        name: "Brand",
+        category: "development",
+        title: "Brex Logo",
+    },
+]
+export const testimonials = [{
+        id: 1,
+        text: "I'm a React Developer. My expertise includes technologies like React.js, Node.js, MongoDB, JavaScript, and TypeScript, C, and C++. I have also honed my problem-solving skills by solving over 200 questions on LeetCode.",
+        image: "./images/testimonials/team-1.jpg",
+        name: "Rishabh Gautam",
+        post: "React Developer",
+    },
+    {
+        id: 2,
+        text: "I'm a Front End Developer. My expertise includes technologies like React.js, Node.js, MongoDB, JavaScript, and TypeScript, C, and C++. I have also honed my problem-solving skills by solving over 250 questions on LeetCode and Codeforces.",
+        image: "./images/testimonials/picname.png",
+        name: "Anshul Kumar",
+        post: "Front End Developer",
+    },
+    {
+        id: 3,
+        text: "I'm a Backend Developer. My expertise includes technologies like React.js, Node.js, MongoDB, JavaScript, and TypeScript, C, and C++. I have also honed my problem-solving skills by solving over 400 questions on LeetCode.",
+        image: "./images/testimonials/picname.png",
+        name: "Amit Chauhan",
+        post: "Backend Developer",
+    },
+]
+export const blog = [{
+        id: 1,
+        title: "Master These Awesome",
+        date: "Jun 27, 2022",
+        author: "Dorian Gray",
+        desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+        cover: "./images/blog/b1.png",
+    },
+    {
+        id: 2,
+        title: "Best Design Items to Appeal",
+        date: "Jun 27, 2022",
+        author: "Dorian Gray",
+        desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+        cover: "./images/blog/b2.png",
+    },
+    {
+        id: 3,
+        title: "The 20 Best Lightroom Presets",
+        date: "Jun 27, 2022",
+        author: "Dorian Gray",
+        desc: "Lorem Ipsum has been standard. Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been",
+        cover: "./images/blog/b3.png",
+    },
+]
+export const contact = [{
+        icon: < AddLocationAltOutlined / > ,
+        text1: "Noida, Delhi",
+        text2: "UP, India",
+    },
+    {
+        icon: < PhoneIphone / > ,
+        text1: "+91-8858913112",
+
+    },
+    {
+        icon: < EmailOutlined / > ,
+        text1: "rishabhgautam21016@gmail.com",
+
+    },
+]
+export const social = [{
+        icon: < Facebook / > ,
+    },
+    {
+        icon: < Twitter / > ,
+    },
+    {
+        icon: < Instagram / > ,
+    },
+    {
+        icon: < YouTube / > ,
+    },
+]
